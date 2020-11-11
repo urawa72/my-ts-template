41ec0183310dea1bb4de62495cb6b5b474508c04
urawa72 <urawa72@gmail.com>
2020-11-11 21:34:22 +0900

    add node type, ignore js file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-const hello = (name: string): string => {
-  return `${name} World!`;
+type B = { hoge: string; fuga: number };
+type DummyB = { hoge: string; fuga: number };
+type ABC = { aa: number[]; bb?: B[]; cc?: { dd: string } };
+const abc: ABC = {
+  aa: [1, 2, 3],
+  bb: [
+    {
+      hoge: 'bbbb',
+      fuga: 11111,
+    },
+  ],
+  // cc: { dd: 'hogeeee' },
 };
 
-console.log(hello('Hello'));
+const result: B[] = [];
+abc.bb?.map((item) => result.push(item));
+console.log(result);
+
+const fuga = (result: B[]): DummyB[] => {
+  const res = result.map((i) => i);
+  return res;
+};
+console.log(fuga(result));
