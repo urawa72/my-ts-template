4450507df4df4fa9adf04a660c23e2bc9fed82a8
urawa72 <urawa72@gmail.com>
2020-11-21 09:39:38 +0900

    delete webpack

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-type B = { hoge: string; fuga: number };
-type DummyB = { hoge: string; fuga: number };
-type ABC = { aa: number[]; bb?: B[]; cc?: { dd: string } };
-const abc: ABC = {
-  aa: [1, 2, 3],
-  bb: [
-    {
-      hoge: 'bbbb',
-      fuga: 11111,
-    },
-  ],
-  // cc: { dd: 'hogeeee' },
-};
+const hello = (msg: string): string => {
+  return `Hello ${msg}!`;
+}
 
-const result: B[] = [];
-abc.bb?.map((item) => result.push(item));
-console.log(result);
-
-const fuga = (result: B[]): DummyB[] => {
-  const res = result.map((i) => i);
-  return res;
-};
-console.log(fuga(result));
+const msg: string = 'World!';
+console.log(hello(msg));
